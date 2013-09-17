ee938c2f32c8d73630e90de2d0544736a486b319
Adam Gerber <adam.gerber@yahoo.com>
2013-09-17 17:00:04 -0500

    initial commit with P0_0 pseudocode

---
+++ src/P0_0.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+/**
+ Name your source files according to its Programming Exercise identifier, except replace the period with underscore.
+ For example, if the Programming Exercise identifier is P1.15, then name your java source file P1_15
+ */
+public class P0_0 {
+
+    public static void main(String[] args) {
+
+        //Example: Find the max value of an indefinite series of integers entered by the user in the command-line
+
+        //pseudocode
+        //while quit is not entered
+            //ask user for integer value or type quit to exit
+            //store integer value in ArrayList
+        //assume the first entry in the ArrayList is the max value
+        //for each value in ArrayList - starting with second entry
+            //if value > max
+                // set max to value
+        //print the max value to the console
+
+    }
+}
