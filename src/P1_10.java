274a06c2697bf9315be7318904d76235cf67de21
Eric Meschke <ericmeschke@gmail.com>
2013-10-01 09:56:55 -0500

    Checking how the commit works.

---
+++ src/P1_10.java
@@ -0,0 +1,28 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: eric
+ * Date: 10/1/13
+ * Time: 9:36 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class P1_10 {
+
+    public static void main(String[] args) {
+        //This is pretty simple, just make a drawing within the text of the print and then have it display
+        // using the System.out.println() command.
+
+        System.out.println("Hello Grader!!" + "/n");
+        System.out.println("    HHHH     HHHH    ");
+        System.out.println("      H       H       ");
+        System.out.println("          >           ");
+        System.out.println("         >>>          ");
+        System.out.println("  XX            XX    ");
+        System.out.println("   XXXX       XXX     ");
+        System.out.println("     XXXXXXXXXX       ");
+        System.out.println("                      ");
+        System.out.println("Simple is Best!!      ");
+
+
+
+    }
+}
