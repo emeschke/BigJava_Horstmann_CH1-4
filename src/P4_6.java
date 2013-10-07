ab9f5781efe1b66f96e3bcd13dbecddf5824cdb9
Eric Meschke <ericmeschke@gmail.com>
2013-10-07 14:34:06 -0500

    The last two problems.

--- src/P4_6.java
+++ src/P4_6.java
@@ -1,5 +1,7 @@
 import com.sun.java_cup.internal.runtime.Scanner;
 
+import java.util.InputMismatchException;
+
 /**
  * Created with IntelliJ IDEA.
  * User: E
@@ -15,25 +17,42 @@ public class P4_6 {
         System.out.println("Enter a non-int to exit):");
         java.util.Scanner in = new java.util.Scanner(System.in);
 
+        //Set boolean first to True.  Initialize other variables.
         Boolean first = Boolean.TRUE;
         int number = 0;
         int minimum = 0;
 
+        //Use a try statement to accept inputs until a non-integer exception triggers the catch.
+        //Within the try use the if statement to change first to False once a minimum is found.
+        //After it is set to false, continue to check for a min.
+        try{
+            while (1 == 1 ) {
+                number = in.nextInt();
+                if(first == Boolean.TRUE){
+                    minimum = number;
+                    first = Boolean.FALSE;
+                }
 
-        while (1 == 1 ) {
-            number = in.nextInt();
-            if(first == Boolean.TRUE){
-                minimum = number;
-                first = Boolean.FALSE;
+                else{
+                    if( number < minimum){
+                        minimum = number;
+                    }
+                }
+            }
+        }
+
+        //Use the catch to kick out if there is a non-int input.  Check whether there is a minimum, indicated
+        //by the first variable being reset.  If there is, print it.
+
+        catch(InputMismatchException ex){
+
+            if (first == Boolean.TRUE){
+                System.out.println("No minimum was found.");
             }
 
             else{
-                if( number < minimum){
-                    minimum = number;
-                }
-            }
             System.out.println("Minimum is " + minimum + ".");
-
+            }
         }
     }
 }
